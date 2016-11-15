9240569eee218c321a07ae95d89a4e125d6736a3
Laurent <lau.cazanove@gmail.com>
2016-11-15 22:15:20 +0100

    Removed foreign key referencing

--- database/migrations/2016_11_15_193841_create_schedules_table.php
+++ database/migrations/2016_11_15_193841_create_schedules_table.php
@@ -16,7 +16,7 @@ class CreateSchedulesTable extends Migration
         Schema::create('schedules', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            //$table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
