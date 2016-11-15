7323356e8714f4a87148b6021e72f2e079beb3b7
Laurent <lau.cazanove@gmail.com>
2016-11-15 20:35:37 +0100

    Games have interested users

--- app/Game.php
+++ app/Game.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    public function interestedUsers()
+    {
+    	return $this->belongsToMany(User::class);
+    }
 }
---
+++ tests/units/GameTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class GameTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testHasInterestedUsers()
+    {
+    	$game = factory(App\Game::class)->create([]);
+        $this->assertNotNull($game->interestedUsers);
+    }
+}
