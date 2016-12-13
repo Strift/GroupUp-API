188d856006ea25a5db5a31a080db4f050beb34cd
Laurent <lau.cazanove@gmail.com>
2016-12-13 13:40:28 +0100

    Edited test to send api token in request parameter

--- tests/features/UserAPITest.php
+++ tests/features/UserAPITest.php
@@ -17,8 +17,8 @@ class UserAPITest extends TestCase
     public function testAdminUserCanList()
     {
         $user = App\User::findByEmail('admin@group-up.com');
-        $this->actingAs($user, 'api')
-            ->get('api/users')
+        $this->actingAs($user)
+            ->get('api/users?api_token=' . $user->api_token)
             ->seeJsonStructure([
                 '*' => ['id', 'name', 'email']
                 ]);
