cbc12b8215be1e0ce8ed170817fc16014bc81eb9
Laurent <lau.cazanove@gmail.com>
2017-01-14 13:47:05 +0100

    Fixed wrong class name

--- tests/components/RegisterAPITest.php
+++ tests/components/RegisterAPITest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class UserAPITest extends TestCase
+class RegisterAPIest extends TestCase
 {
     use DatabaseMigrations;
 
