4db8f3ed4bb34d2659f4f3b0bc30beaa1ad98392
Laurent <lau.cazanove@gmail.com>
2017-01-15 20:38:01 +0100

    Refactoring finished

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -15,54 +15,46 @@ use Illuminate\Support\Facades\Lang;
 
 class LoginController extends Controller
 {
-	use AuthenticatesUsers;
+    use AuthenticatesUsers;
 
-	protected $username;
+    protected $username;
 
-	public function login(Request $request)
+    public function login(Request $request)
     {
-    	try
-    	{
-    		// Define the username used for credentials
-    		$this->username = $request->has('username') ? 'username' : 'email';
+        try
+        {
+            // Define the username used for credentials
+            $this->username = $request->has('username') ? 'username' : 'email';
 
-    		// Validation
-    		$validator = $this->validator($request->only(['email', 'password']));
-    		if ($validator->fails())
-    		{
-    			return response()->error($validator->messages(), 422);
-    		}
+            // Validation
+            $validator = $this->validator($request->only(['email', 'password']));
+            if ($validator->fails()) {
+                return response()->error($validator->messages(), 422);
+            }
 
-    		// If the class is using the ThrottlesLogins trait, we can automatically throttle
-	        // the login attempts for this application. We'll key this by the username and
-	        // the IP address of the client making these requests into this application.
-	        if ($this->hasTooManyLoginAttempts($request)) {
-	            $this->fireLockoutEvent($request);
-	            return $this->sendLockoutResponse($request);
-	        }
+            // If the class is using the ThrottlesLogins trait, we can automatically throttle
+            // the login attempts for this application. We'll key this by the username and
+            // the IP address of the client making these requests into this application.
+            if ($this->hasTooManyLoginAttempts($request)) {
+                $this->fireLockoutEvent($request);
+                return $this->sendLockoutResponse($request);
+            }
 
-	        if ($this->attemptLogin($request)) 
-	        {
-	        	$user = Auth::user();
-	        	return response()->success($user->makeVisible('api_token')->toArray());
-            	//return $this->sendLoginResponse($request);
+            if ($this->attemptLogin($request)) {
+                return $this->sendLoginResponse($request);
             }
 
             // If the login attempt was unsuccessful we will increment the number of attempts
-	        // to login and redirect the user back to the login form. Of course, when this
-	        // user surpasses their maximum number of attempts they will get locked out.
-	        $this->incrementLoginAttempts($request);
+            // to login and redirect the user back to the login form. Of course, when this
+            // user surpasses their maximum number of attempts they will get locked out.
+            $this->incrementLoginAttempts($request);
 
-	        return response()->error([
-	        						'input' => $request->only('email'),
-	        						$this->username() => Lang::get('auth.failed')
-	        						], 500);
-	        //return $this->sendFailedLoginResponse($request);
-    	}
-    	catch (Exception $e)
-    	{
-    		return response()->error($e->getMessage(), 500);
-    	}
+            return $this->sendFailedLoginResponse($request);
+        }
+        catch (Exception $e)
+        {
+            return response()->error($e->getMessage(), 500);
+        }
     }
 
     protected function validator(array $data)
@@ -75,25 +67,20 @@ class LoginController extends Controller
 
     protected function sendLoginResponse(Request $request)
     {
-        $request->session()->regenerate();
-
         $this->clearLoginAttempts($request);
-
-        return $this->authenticated($request, $this->guard()->user())
-                ?: redirect()->intended($this->redirectPath());
+        return response()->success($this->guard()->user()->makeVisible('api_token')->toArray());
     }
 
     protected function sendFailedLoginResponse(Request $request)
     {
-        return redirect()->back()
-            ->withInput($request->only($this->username(), 'remember'))
-            ->withErrors([
-                $this->username() => Lang::get('auth.failed'),
-            ]);
+        return response()->error([
+                                'input' => $request->only('email'),
+                                $this->username() => Lang::get('auth.failed')
+                                ], 500);
     } 
 
     protected function username()
     {
-    	return $this->username;
+        return $this->username;
     }
 }
