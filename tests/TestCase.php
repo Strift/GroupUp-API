116ecf088d826b4b70c80a4fa18d35fa25341e44
Laurent <lau.cazanove@gmail.com>
2017-01-27 19:08:57 +0100

    Included trait dependency

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+use Tests\CreatesApplication;
+
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
