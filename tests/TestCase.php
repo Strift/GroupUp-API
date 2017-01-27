181d24eea72d3a0905999e06fab57c37e069b825
Laurent <lau.cazanove@gmail.com>
2017-01-27 21:05:46 +0100

    Upgraded TestCase to 5.4

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,6 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-use Tests\CreatesApplication;
-
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
