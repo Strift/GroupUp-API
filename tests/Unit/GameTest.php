1d02d649d3e39b80822354655646375d1bd24475
Laurent <lau.cazanove@gmail.com>
2017-01-27 20:44:52 +0100

    Upgraded Game tests to 5.4

--- tests/Unit/GameTest.php
+++ tests/Unit/GameTest.php
@@ -2,8 +2,7 @@
 
 namespace Tests\Unit;
 
-use Tests\BrowserKitTest as TestCase;
-
+use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
