83e344ef4e6cb2a5ca21dc00753276641a382c5b
Laurent <lau.cazanove@gmail.com>
2017-01-27 19:43:59 +0100

    Upgraded Users API to 5.4

--- tests/Feature/Api/UsersControllerTest.php
+++ tests/Feature/Api/UsersControllerTest.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Api;
 
-use Tests\BrowserKitTest as TestCase;
-
+use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -24,20 +23,20 @@ class UserAPITest extends TestCase
     {
         $user = User::findByEmail('admin@group-up.com');
         $this->get('api/users?api_token=' . $user->api_token)
-            ->seeJsonStructure([
-                'errors',
-                'data' => [
+            ->assertJson([
+                'errors' => false,
+                'data' => [/* Unsupported since Laravel 5.4
                     '*' => ['id', 'username', 'email', 'friends']
-                    ]
+                    */]
                 ])
-            ->seeStatusCode(200);
+            ->assertStatus(200);
     }
 
     public function testStandardUserCannotList()
     {
         $user = factory(User::class)->create([]);
         $this->get('api/users?api_token=' . $user->api_token)
-            ->seeStatusCode(403);
+            ->assertStatus(403);
     }
 
     public function testAdminUserCanView()
@@ -45,22 +44,30 @@ class UserAPITest extends TestCase
         $adminUser = User::findByEmail('admin@group-up.com');
         $user = factory(User::class)->create();
         $this->get('api/users/' . $user->id . '?api_token=' . $adminUser->api_token)
-            ->seeJsonStructure([
-                'errors',
-                'data' => ['id', 'username', 'email']
+            ->assertJson([
+                'errors' => false,
+                'data' => [
+                	'id' => $user->id, 
+                	'username' => $user->username, 
+                	'email'=> $user->email
+                	]
                 ])
-            ->seeStatusCode(200);
+            ->assertStatus(200);
     }
 
     public function testUserCanViewHimself()
     {
         $user = factory(User::class)->create([]);
         $this->get('api/users/' . $user->id . '?api_token=' . $user->api_token)
-            ->seeJsonStructure([
-                'errors',
-                'data' => ['id', 'username', 'email']
+            ->assertJson([
+                'errors' => false,
+                'data' => [
+                	'id' => $user->id, 
+                	'username' => $user->username, 
+                	'email'=> $user->email
+                	]
                 ])
-            ->seeStatusCode(200);
+            ->assertStatus(200);
     }
 
     public function testUserCannotViewOthers()
@@ -68,14 +75,14 @@ class UserAPITest extends TestCase
         $user1 = factory(User::class)->create([]);
         $user2 = factory(User::class)->create([]);
         $this->get('api/users/' . $user1->id . '?api_token=' . $user2->api_token)
-            ->seeStatusCode(403);
+            ->assertStatus(403);
     }
 
     public function testUserCanDeleteHimself()
     {
         $user = factory(User::class)->create([]);
         $this->delete('api/users/' . $user->id . '?api_token=' . $user->api_token)
-            ->seeStatusCode(200);
+            ->assertStatus(200);
     }
 
     public function testUserCannotDeleteOthers()
@@ -83,6 +90,6 @@ class UserAPITest extends TestCase
         $user1 = factory(User::class)->create([]);
         $user2 = factory(User::class)->create([]);
         $this->delete('api/users/' . $user1->id . '?api_token=' . $user2->api_token)
-            ->seeStatusCode(403);
+            ->assertStatus(403);
     }
 }
