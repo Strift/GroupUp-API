a0906e125258cb127bb8289ae876ed15a2f0159a
Laurent <lau.cazanove@gmail.com>
2017-01-30 11:57:31 +0100

    Verification token is fillable

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'password', 'api_token', 'verified_at'
+        'username', 'email', 'password', 'api_token', 'verification_token'
     ];
 
     /**
